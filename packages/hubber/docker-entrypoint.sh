f224f9906618d08daf1c5817fd0489e96539b3f1
Jun Jiang <jasl9187@hotmail.com>
2023-12-13 04:04:13 +0800

    Add hubber

--- packages/hubber/docker-entrypoint.sh
+++ packages/hubber/docker-entrypoint.sh
@@ -7,4 +7,7 @@ cd /root
 echo "Starting JupyterLab @ http://$(hostname -I | cut -d' ' -f1):8888 (password nvidia)"
 echo "JupterLab logging location:  /var/log/jupyter.log  (inside the container)"
 
-jupyter lab --ip 0.0.0.0 --port 8888 --allow-root "$@" >> /var/log/jupyter.log 2>&1
+# jupyter lab --ip 0.0.0.0 --port 8888 --allow-root "$@" >> /var/log/jupyter.log 2>&1
+
+jupyter lab --ip 0.0.0.0 --port 8888 --allow-root &> /var/log/jupyter.log &
+exec "$@"
