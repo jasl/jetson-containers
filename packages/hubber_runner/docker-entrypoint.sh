a263d1d0587f915978825a7818e303398af468fd
Jun Jiang <jasl9187@hotmail.com>
2023-12-15 07:09:46 +0800

    Add hubber_runner

---
+++ packages/hubber_runner/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd /root
+
+export JUPYTER_DATA_DIR=/root/jupyter
+mkdir -p $JUPYTER_DATA_DIR
+
+echo "Starting JupyterLab @ http://$(hostname -I | cut -d' ' -f1):8888 (password nvidia)"
+echo "JupterLab logging location:  /var/log/jupyter.log  (inside the container)"
+
+# jupyter lab --ip 0.0.0.0 --port 8888 --allow-root "$@" >> /var/log/jupyter.log 2>&1
+
+jupyter lab --ip 0.0.0.0 --port 8888 --allow-root &> /var/log/jupyter.log &
+exec "$@"
